3ffc2d7005a303dd1966cf27db7ba54624938cfa
Anjana <sinhaanjana32@gmail.com>
2020-06-05 01:17:40 +0300

    Updates

---
+++ precache-manifest.e2bb6c353b27570e9049cdce18d50c4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0995b1b96def51744331dec6ed86b94d",
+    "url": "/React-Chart1/index.html"
+  },
+  {
+    "revision": "1e1440d254b5e9bce136",
+    "url": "/React-Chart1/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "5518aa3e47f4591c89f3",
+    "url": "/React-Chart1/static/js/2.e1d5807c.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/React-Chart1/static/js/2.e1d5807c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1440d254b5e9bce136",
+    "url": "/React-Chart1/static/js/main.8e410e1e.chunk.js"
+  },
+  {
+    "revision": "ba0a4431c8b78badce4e",
+    "url": "/React-Chart1/static/js/runtime-main.a0e4842e.js"
+  }
+]);
